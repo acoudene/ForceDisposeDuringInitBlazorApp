5aa628f942eb16c86140a2e9cc4b5810fa6a5fe7
Anthony COUDENE <anthony.coudene@technidata-web.com>
2023-02-13 11:57:41 +0100

    Add project files.

---
+++ ForceDisposeDuringInitBlazorApp/Client/Services/NotificationService.cs
@@ -0,0 +1,13 @@
+﻿namespace ForceDisposeDuringInitBlazorApp.Client.Services
+{
+  public class NotificationService
+  {
+    public event EventHandler? NotifiedEvent;    
+
+    public async Task NotifyAsync()
+    {
+      NotifiedEvent?.Invoke(this, new EventArgs());
+      await Task.CompletedTask;
+    }
+  }
+}
---
+++ ForceDisposeDuringInitBlazorApp/Client/wwwroot/js/DomInfo.js
@@ -0,0 +1,20 @@
+﻿ function GetWidthByElementId(elementId) {
+    return document.getElementById(elementId).clientWidth;
+}
+function GetHeightByElementId(elementId) {
+    return document.getElementById(elementId).clientHeight;
+}
+function GetLeftPositionByElementId(elementId) {
+    return document.getElementById(elementId).getBoundingClientRect().left;
+}
+function GetBottomPositionByElementId(elementId) {
+    return document.getElementById(elementId).getBoundingClientRect().bottom;
+}
+function OpenNewTab(url) {
+        const link = document.createElement('a');
+        link.href = url;
+        link.target = '_blank';
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+}
